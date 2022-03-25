44fdf0f23c796112a337e60e330490710ec00773
hazemshikhon <zuma433@gmail.com>
2022-03-25 15:29:59 +0200

    Splash Screen

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
-import { Router, Stack, Scene, Actions } from 'react-native-router-flux';
+import { Router, Stack, Scene } from 'react-native-router-flux';
 import VacationRequestsController from './app/controllers/VacationRequestsController';
-
+import Splash from './app/screens/Splash';
 import CreateVacationController from './app/controllers/CreateVacationController';
 const App = () => {
   return (
     <Router>
       <Stack key="root">
+        <Scene key="Splash" component={Splash} title="Splash" hideNavBar initial />
         <Scene key="VacationRequestsController" component={VacationRequestsController} title="VacationRequestsController" hideNavBar />
-        <Scene key="CreateVacationController" component={CreateVacationController} title="CreateVacationController" hideNavBar initial />
+        <Scene key="CreateVacationController" component={CreateVacationController} title="CreateVacationController" hideNavBar />
       </Stack>
     </Router>
   );
---
+++ app/screens/Splash.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import { View, StyleSheet, StatusBar, Text } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import colors from '../config/colors';
+
+const Splash = ({}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      Actions.VacationRequestsController();
+    }, 2500);
+  }, []);
+  return (
+    <View style={styles.container}>
+      <StatusBar backgroundColor={colors.primary} barStyle="light-content" />
+      <Text style={styles.mainText}> VACATION APP</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.primary,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+  },
+  mainText: {
+    color: colors.white,
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
+
+export default Splash;
